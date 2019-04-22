5ee585dd8c96be8749cac0103b807ee211ccac8c
Diego Rodriguez <diegohumanista@gmail.com>
2019-04-22 01:28:12 -0300

    Agregada constribución de @L4Vo5
    @L4Vo5 simplificó la clase y le cambió a rutina de detección de ganador por "magia negra" XD

--- ta-te-ti.py
+++ ta-te-ti.py
@@ -1,108 +1,51 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr 18 23:14:32 2019
-
-@author: Diego
-"""
-
 class tateti:
-    def __init__(self):
+    def __init__(self,orden = 'XO'):
         self.__celdas = [' ', ' ', ' ', 
                        ' ', ' ', ' ',
                        ' ', ' ', ' ']
         self.__quienGano = ''
-        self.__turno = ''
-        
-    def PonerX(self, posicion):
-        if self.__turno == '':
-            self.__turno = 'X'
-        if self.__turno != 'X':
-            print('No se puede poner X: es el turno de O')
-            return False
+        self.__fichas = orden
+        self.__turno = orden[0]
+    def PonerFicha(self, posicion):
         if self.hayGanador:
-            print('No se puede poner X: ya hay un ganador')
+            print('No se puede poner ficha: ya hay un ganador')
             return False
         if self.__celdas[posicion] == ' ':
-            self.__celdas[posicion] = 'X'
+            self.__celdas[posicion] = self.__turno
             self.__calcularGanador()
-            self.__turno = 'O'
+            self.__turno = self.__fichas[self.__fichas.find(self.__turno)-1]
             return True
         else:
-            print('No se puede poner X: casilla ocupada')
-            return False
-    def PonerO(self, posicion):
-        if self.__turno == '':
-            self.__turno = 'O'
-        if self.__turno != 'O':
-            print('No se puede poner O: es el turno de X')
-            return False
-        if self.hayGanador:
-            print('No se puede poner O: ya hay un ganador')
-            return False
-        if self.__celdas[posicion] == ' ':
-            self.__celdas[posicion] = 'O'
-            self.__calcularGanador()
-            self.__turno = 'X'
-            return True
-        else:
-            print('No se puede poner O: casilla ocupada')
+            print('No se puede poner ficha: casilla ocupada')
             return False
     def getHayGanador(self):
         return self.__quienGano != ''
     hayGanador = property(getHayGanador)
- 
     def getQuienGano(self):
         return self.__quienGano
     quienGano = property(getQuienGano)
-
+    def setQuienGano(self, val):
+        self.__quienGano = val
+    def getCeldas(self):
+        return self.__celdas
+    celdas = property(getCeldas)
+    def reset(self):
+        self.__celdas = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
+        self.__quienGano = ''
+        self.__turno = ''
+    def partidaRapida(self, movidas): #Función de testeo
+        for m in movidas:
+            self.PonerFicha(m)
     def __calcularGanador(self):
-        if self.__celdas[0] + self.__celdas[1] + self.__celdas[2] == 'OOO':
-            self.__quienGano = self.__celdas[0]
-        if self.__celdas[3] + self.__celdas[4] + self.__celdas[5] == 'OOO':
-            self.__quienGano = self.__celdas[3]
-        if self.__celdas[6] + self.__celdas[7] + self.__celdas[8] == 'OOO':
-            self.__quienGano = self.__celdas[6]
-        if self.__celdas[0] + self.__celdas[3] + self.__celdas[6] == 'OOO':
-            self.__quienGano = self.__celdas[0]
-        if self.__celdas[1] + self.__celdas[4] + self.__celdas[7] == 'OOO':
-            self.__quienGano = self.__celdas[1]
-        if self.__celdas[2] + self.__celdas[5] + self.__celdas[8] == 'OOO':
-            self.__quienGano = self.__celdas[2]
-        if self.__celdas[0] + self.__celdas[4] + self.__celdas[8] == 'OOO':
-            self.__quienGano = self.__celdas[0]
-        if self.__celdas[2] + self.__celdas[4] + self.__celdas[6] == 'OOO':
-            self.__quienGano = self.__celdas[2]
- 
-        if self.__celdas[0] + self.__celdas[1] + self.__celdas[2] == 'XXX':
-            self.__quienGano = self.__celdas[0]
-        if self.__celdas[3] + self.__celdas[4] + self.__celdas[5] == 'XXX':
-            self.__quienGano = self.__celdas[3]
-        if self.__celdas[6] + self.__celdas[7] + self.__celdas[8] == 'XXX':
-            self.__quienGano = self.__celdas[6]
-        if self.__celdas[0] + self.__celdas[3] + self.__celdas[6] == 'XXX':
-            self.__quienGano = self.__celdas[0]
-        if self.__celdas[1] + self.__celdas[4] + self.__celdas[7] == 'XXX':
-            self.__quienGano = self.__celdas[1]
-        if self.__celdas[2] + self.__celdas[5] + self.__celdas[8] == 'XXX':
-            self.__quienGano = self.__celdas[2]
-        if self.__celdas[0] + self.__celdas[4] + self.__celdas[8] == 'XXX':
-            self.__quienGano = self.__celdas[0]
-        if self.__celdas[2] + self.__celdas[4] + self.__celdas[6] == 'XXX':
-            self.__quienGano = self.__celdas[2]
-         
-        
+        __array = [0,1,3,1,6,1,0,3,1,3,2,3,0,4,2,2] #Magia negra
+        for n in range(8):
+            init = __array[n*2]
+            incr = __array[n*2+1]
+            if (self.__celdas[init] == self.__celdas[init+incr] == self.__celdas[init+incr*2]) and (self.__celdas[init] != ' '):
+                self.__quienGano = self.__celdas[__array[n*2]]
+        if self.__quienGano == '' and not " " in self.__celdas:
+            self.__quienGano = "Empate"
     def __str__(self):
         return "[{} {} {}]\n[{} {} {}]\n[{} {} {}]".format(self.__celdas[0],self.__celdas[1],self.__celdas[2],self.__celdas[3],self.__celdas[4],self.__celdas[5],self.__celdas[6],self.__celdas[7],self.__celdas[8])
     def __repr__(self):
         return str(self)
-'''
-# para testear como funciona
-p = tateti()
-p.PonerO(1)
-p.PonerX(3)
-p.PonerO(4)
-p.PonerX(5)
-p.PonerO(7)
-print(p)
-print(p.hayGanador)
-'''
